1140864b42061daf0ada605b236c1f7a9947317e
newyork167 <newyork167@gmail.com>
2024-05-16 11:37:06 -0400

    Added checks to setup to make venv, source, and setup pycyling as needed

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
-python setup.py develop
+#!/bin/bash
+
+# Check if venv directory exists
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activate venv based on OS
+if [[ "$OSTYPE" == "msys" ]]; then
+    source venv/Scripts/activate
+else
+    source venv/bin/activate
+fi
+
+# Run setup.py
+python pycycling/setup.py develop
